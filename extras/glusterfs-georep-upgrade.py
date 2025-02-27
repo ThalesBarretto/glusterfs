60206b2c502520731e3c493f20ab22673ddbda39
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-02-26 21:50:12 -0300

    python linter: clean extras/glusterfs-georep-upgrade.py
            This series aim to reduce the python linter build logs.
    
            This commit silences most of the python linter complaints
    without any functional changes. The changes include mostly formatting
    and missing obvious docstrings. We left out dependency and exception
    handling complaints on purpose to be addressed separately.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
    #  extras/glusterfs-georep-upgrade.py | 41 ++++++++++++++++++++++----------------
    #  1 file changed, 24 insertions(+), 17 deletions(-)
    #  extras/glusterfs-georep-upgrade.py | 41 ++++++++++++++++++++++----------------
    #  1 file changed, 24 insertions(+), 17 deletions(-)
    #  extras/glusterfs-georep-upgrade.py | 41 ++++++++++++++++++++++----------------
    #  1 file changed, 24 insertions(+), 17 deletions(-)
    #  extras/glusterfs-georep-upgrade.py | 41 ++++++++++++++++++++++----------------
    #  1 file changed, 24 insertions(+), 17 deletions(-)

--- extras/glusterfs-georep-upgrade.py
+++ extras/glusterfs-georep-upgrade.py
@@ -13,19 +13,22 @@ cases as published by the Free Software Foundation.
 
 import argparse
 import errno
-import os, sys
+import os
 import shutil
+import sys
 from datetime import datetime
+
 
 def find_htime_path(brick_path):
     dirs = []
     htime_dir = os.path.join(brick_path, '.glusterfs/changelogs/htime')
     for file in os.listdir(htime_dir):
-        if os.path.isfile(os.path.join(htime_dir,file)) and file.startswith("HTIME"):
+        if os.path.isfile(os.path.join(htime_dir, file)) and file.startswith("HTIME"):
             dirs.append(os.path.join(htime_dir, file))
         else:
             raise FileNotFoundError("%s unavailable" % (os.path.join(htime_dir, file)))
     return dirs
+
 
 def modify_htime_file(brick_path):
     htime_file_path_list = find_htime_path(brick_path)
@@ -34,24 +37,25 @@ def modify_htime_file(brick_path):
         changelog_path = os.path.join(brick_path, '.glusterfs/changelogs')
         temp_htime_path = os.path.join(changelog_path, 'htime/temp_htime_file')
         with open(htime_file_path, 'r') as htime_file, open(temp_htime_path, 'w') as temp_htime_file:
-            #extract epoch times from htime file
+            # extract epoch times from htime file
             paths = htime_file.read().split("\x00")
 
             for pth in paths:
                 epoch_no = pth.split(".")[-1]
                 changelog = os.path.basename(pth)
-                #convert epoch time to year, month and day
+                # convert epoch time to year, month and day
                 if epoch_no != '':
-                    date=(datetime.fromtimestamp(float(int(epoch_no))).strftime("%Y/%m/%d"))
-                    #update paths in temp htime file
+                    date = (datetime.fromtimestamp(float(int(epoch_no))).strftime("%Y/%m/%d"))
+                    # update paths in temp htime file
                     temp_htime_file.write("%s/%s/%s\x00" % (changelog_path, date, changelog))
-                    #create directory in the format year/month/days
+                    # create directory in the format year/month/days
                     path = os.path.join(changelog_path, date)
 
                 if changelog.startswith("CHANGELOG."):
-                    os.makedirs(path, mode = 0o600, exist_ok = True)
+                    os.makedirs(path, mode=0o600, exist_ok=True)
 
-                    #copy existing changelogs to new directory structure, delete old changelog files
+                    # copy existing changelogs to new directory structure,
+                    # delete old changelog files
                     try:
                         shutil.copyfile(pth, os.path.join(path, changelog))
                     except shutil.SameFileError:
@@ -59,17 +63,20 @@ def modify_htime_file(brick_path):
                     else:
                         os.remove(pth)
 
-        #rename temp_htime_file with htime file
-        os.rename(htime_file_path, os.path.join('%s.bak'%htime_file_path))
+        # rename temp_htime_file with htime file
+        os.rename(htime_file_path, os.path.join('%s.bak' % htime_file_path))
         os.rename(temp_htime_path, htime_file_path)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('brick_path', help="This upgrade script, which is to be run on\
-                         server side, takes brick path as the argument, \
-                         updates paths inside htime file and alters the directory structure \
-                         above the changelog files inorder to support new optimised format \
-                         of the directory structure as per \
-                         https://review.gluster.org/#/c/glusterfs/+/23733/")
+    parser.add_argument('brick_path',
+                        help="This upgrade script, which is to be run on\
+                        server side, takes brick path as the argument, \
+                        updates paths inside htime file and alters the \
+                        directory structure above the changelog files in \
+                        order to support new optimised format of the  \
+                        directory structure as per \
+                        https://review.gluster.org/#/c/glusterfs/+/23733/")
     args = parser.parse_args()
     modify_htime_file(args.brick_path)
