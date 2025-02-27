067ea54eb02788e9d79d537dde6475a497178934
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-02-26 21:44:57 -0300

    python linter: clean xlators/features/changelog/lib/examples/python/libgfchangelog.py
            This series aim to reduce the python linter build logs.
    
            This commit silences most of the python linter complaints
    without any functional changes. The changes include mostly formatting
    and missing obvious docstrings. We left out dependency and exception
    handling complaints on purpose to be addressed separately.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>

--- xlators/features/changelog/lib/examples/python/libgfchangelog.py
+++ xlators/features/changelog/lib/examples/python/libgfchangelog.py
@@ -2,6 +2,7 @@ import os
 from ctypes import *
 from ctypes.util import find_library
 
+
 class Changes(object):
     libgfc = CDLL(find_library("gfchangelog"), mode=RTLD_GLOBAL,
                   use_errno=True)
@@ -26,9 +27,10 @@ class Changes(object):
             cls.raise_changelog_err()
 
     @classmethod
-    def cl_register(cls, brick, path, log_file, log_level, retries = 0):
+    def cl_register(cls, brick, path, log_file, log_level, retries=0):
         ret = cls._get_api('gf_changelog_register')(brick, path,
-                                                    log_file, log_level, retries)
+                                                    log_file, log_level,
+                                                    retries)
         if ret == -1:
             cls.raise_oserr()
 
@@ -46,7 +48,7 @@ class Changes(object):
 
     @classmethod
     def cl_getchanges(cls):
-        """ remove hardcoding for path name length """
+        """Remove hardcoding for path name length."""
         def clsort(f):
             return f.split('.')[-1]
         changes = []
@@ -56,7 +58,7 @@ class Changes(object):
         while True:
             ret = call(buf, 4096)
             if ret in (0, -1):
-                break;
+                break
             changes.append(buf.raw[:ret-1])
         if ret == -1:
             cls.raise_oserr()
