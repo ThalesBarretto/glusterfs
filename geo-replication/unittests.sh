c44a3bd39d9d71a36fd2c0b02c481367a57f57d7
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-03-07 12:57:47 -0300

    shellcheck linter: use bash geo-replication/unittests.sh
    	This commit changes the shell to #!/usr/bin/env bash.
    
            This series aim to reduce the shellcheck logs.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>

--- geo-replication/unittests.sh
+++ geo-replication/unittests.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname $0)"/tests/unit
 nosetests -v --exe --with-coverage --cover-package \
-          syncdaemon --cover-erase --cover-html --cover-branches $@
+          syncdaemon --cover-erase --cover-html --cover-branches "$@"
 
 saved_status=$?
 rm -f .coverage
