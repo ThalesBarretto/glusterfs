76084342e2df55cb7858843ead00777127f03570
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-03-06 22:44:02 -0300

    shellcheck linter: use bash extras/benchmarking/launch-script.sh
    	This commit changes the shell to #!/usr/bin/env bash.
    
            This series aim to reduce the shellcheck logs.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
    #  extras/benchmarking/launch-script.sh | 4 ++--
    #  1 file changed, 2 insertions(+), 2 deletions(-)
    #  extras/benchmarking/launch-script.sh | 4 ++--
    #  1 file changed, 2 insertions(+), 2 deletions(-)
    #  extras/benchmarking/launch-script.sh | 4 ++--
    #  1 file changed, 2 insertions(+), 2 deletions(-)

--- extras/benchmarking/launch-script.sh
+++ extras/benchmarking/launch-script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # This script is to launch the script in parallel across all the nodes.
 
@@ -8,7 +8,7 @@ path_to_script="$mount_point}/benchmark/local-script.sh"
 num_hosts=8
 
 for i in $(seq 1 $num_hosts); do 
-    ssh node$i path_to_script &
+    ssh "node${i}" path_to_script &
 done
 
 sleep 3;
