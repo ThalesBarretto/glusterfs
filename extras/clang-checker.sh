eb60cb19490b3435527795594cfb4ff4013db7b7
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-03-07 12:47:53 -0300

    shellcheck linter: use bash extras/clang-checker.sh
    	This commit changes the shell to #!/usr/bin/env bash.
    
            This series aim to reduce the shellcheck logs.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
    #  extras/clang-checker.sh | 142 ++++++++++++++++++++++++------------------------
    #  1 file changed, 71 insertions(+), 71 deletions(-)
    #  extras/clang-checker.sh | 142 ++++++++++++++++++++++++------------------------
    #  1 file changed, 71 insertions(+), 71 deletions(-)
    #  extras/clang-checker.sh | 142 ++++++++++++++++++++++++------------------------
    #  1 file changed, 71 insertions(+), 71 deletions(-)

--- extras/clang-checker.sh
+++ extras/clang-checker.sh
@@ -41,18 +41,18 @@ TBACKUP_DIR=${TARGET_DIR}/backup
 
 declare -A DICT_B
 declare -A DICT_T
-declare -A ARR
-declare -A FILES
+declare -a ARR
+declare -a FILES
 
 function identify_changes () {
     MODIFIED_DATA=$(git show --name-status --oneline | tail -n +2)
     FLAG=0
     for i in ${MODIFIED_DATA}; do
         if [ $FLAG -eq 1 ]; then
-            ARR+="$(dirname $i) ";
+		ARR+=( "$(dirname "${i}") " );
             FLAG=0;
         fi
-        if [ $i = 'M' ] || [ $i = 'A' ]; then
+        if [ "${i}" = 'M' ] || [ "${i}" = 'A' ]; then
             FLAG=1;
         fi
     done
@@ -60,14 +60,14 @@ function identify_changes () {
     MODIFIED_DIR=$(echo "${ARR[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
     for i in $MODIFIED_DIR; do
         # run only in directories which has Makefile
-        if [ $(find ./$i -iname "makefile*" | wc -c) -gt 0 ]; then
+        if [ "$(find "./${i}" -iname "makefile*" | wc -c)" -gt 0 ]; then
             # skip 'doc' and '.'(top) directory
             if [ "xx$i" != "xxdoc" ] && [ "xx$i" != "xx." ]; then
-                FILES+="$i "
+		    FILES+=( "$i " )
             fi
         fi
     done
-    if [ -z $FILES ]; then
+    if [ -z "${FILES}" ]; then
         echo "Probably no changes made to 'c' files"
         exit;
     fi
@@ -76,12 +76,12 @@ function identify_changes () {
 function check_prerequisites () {
     if ! type "clang" 2> /dev/null; then
         echo -e "\ntry after installing clang and scan-build..."
-        echo    "useful info at http://clang-analyzer.llvm.org/installation.html\n"
+        echo -e "useful info at http://clang-analyzer.llvm.org/installation.html\n"
         echo -e "hint: 'dnf -y install clang-analyzer.noarch'\n"
         exit 1;
     elif ! type "scan-build" 2> /dev/null; then
         echo -e "\ntry after installing scan-build..."
-        echo    "useful info at http://clang-analyzer.llvm.org/installation.html"
+        echo -e "useful info at http://clang-analyzer.llvm.org/installation.html"
         echo -e "hint: 'dnf -y install clang-analyzer.noarch'\n"
         exit 1;
     fi
@@ -90,8 +90,8 @@ function check_prerequisites () {
 function force_terminate () {
     echo -e "\nreceived a signal to force terminate ..\n"
     git am --abort 2> /dev/null
-    git am ${PATCH_NAME}
-    rm -f ${REPORTS_DIR}/${PATCH_NAME}
+    git am "${PATCH_NAME}"
+    rm -f "${REPORTS_DIR}/${PATCH_NAME}"
     exit 1;
 }
 
@@ -106,15 +106,15 @@ function run_scanbuild () {
     echo -e "Changes are identified in '${FILES[@]}' directorie[s]\n"
 
     if [ -d "${BRESULTS_DIR}" ]; then
-        mkdir -p ${BBACKUP_DIR} ${TBACKUP_DIR}
-        mv ${BRESULTS_DIR} \
-           ${BBACKUP_DIR}/results_$(ls -l ${BBACKUP_DIR} | wc -l)
-        mv ${TRESULTS_DIR} \
-           ${TBACKUP_DIR}/results_$(ls -l ${TBACKUP_DIR} | wc -l)
+        mkdir -p "${BBACKUP_DIR}" "${TBACKUP_DIR}"
+        mv "${BRESULTS_DIR}" \
+           "${BBACKUP_DIR}/results_$(ls -l ${BBACKUP_DIR} | wc -l)"
+        mv "${TRESULTS_DIR}" \
+           "${TBACKUP_DIR}/results_$(ls -l ${TBACKUP_DIR} | wc -l)"
     fi
     for DIR in ${FILES[@]}; do
-        mkdir -p ${BRESULTS_DIR}/$(echo ${DIR} | sed 's/\//_/g')
-        mkdir -p ${TRESULTS_DIR}/$(echo ${DIR} | sed 's/\//_/g')
+        mkdir -p "${BRESULTS_DIR}/$(echo ${DIR} | sed 's/\//_/g')"
+        mkdir -p "${TRESULTS_DIR}/$(echo ${DIR} | sed 's/\//_/g')"
     done
     # get nproc info
     case $(uname -s) in
@@ -132,110 +132,110 @@ function run_scanbuild () {
 
     # build complete source code for sake of dependencies
     echo -e "\n# make -j${NPROC} ..."
-    make -j${NPROC} 1>/dev/null
+    make -j"${NPROC}" 1>/dev/null
 
-    for DIR in ${FILES[@]}; do
-        if [ $(find ./$i -iname "makefile*" | wc -c) -gt 0 ]; then
-            make clean -C ${DIR} 1>/dev/null
+    for DIR in "${FILES[@]}" ; do
+        if [ "$(find "./${i}" -iname "makefile*" | wc -c)" -gt 0 ]; then
+            make clean -C "${DIR}" 1>/dev/null
             echo -e "\n| Analyzing ${DIR} without commit ... |\n"
             # run only in directory where changes are made
-            ${SCAN_BUILD} -o ${BRESULTS_DIR}/$(echo ${DIR} | sed 's/\//_/g') \
-                          --use-analyzer=${CLANG} make -j${NPROC} -C ${DIR}
+            ${SCAN_BUILD} -o "${BRESULTS_DIR}/$(echo "${DIR}" | sed 's/\//_/g')" \
+                          --use-analyzer="${CLANG}" make -j"${NPROC}" -C "${DIR}"
         fi
     done
 
     echo -e "\n| Analyzing without commit complete ... |\n"
 
-    git am ${PATCH_NAME}
+    git am "${PATCH_NAME}"
     trap - INT TERM QUIT EXIT
 
     # In case commit has changes to configure stuff ?
     echo -e "\n# make clean ..."
     make clean 1>/dev/null
     echo -e "\n# ./autogen.sh && ./configure --with-previous-options ..."
-    ${REPORTS_DIR}/autogen.sh 2>/dev/null
-    ${REPORTS_DIR}/configure  --with-previous-options 1>/dev/null
+    "${REPORTS_DIR}"/autogen.sh 2>/dev/null
+    "${REPORTS_DIR}"/configure  --with-previous-options 1>/dev/null
     echo -e "\n# make -j${NPROC} ..."
-    make -j${NPROC} 1>/dev/null
+    make -j"${NPROC}" 1>/dev/null
 
-    for DIR in ${FILES[@]}; do
-        if [ $(find ./$i -iname "makefile*" | wc -c) -gt 0 ]; then
-            make clean -C ${DIR} 1>/dev/null
+    for DIR in "${FILES[@]}" ; do
+        if [ "$(find "./${i}" -iname "makefile*" | wc -c)" -gt 0 ]; then
+            make clean -C "${DIR}" 1>/dev/null
             echo -e "\n| Analyzing ${DIR} with commit ... |\n"
             # run only in directory where changes are made
-            ${SCAN_BUILD} -o ${TRESULTS_DIR}/$(echo ${DIR} | sed 's/\//_/g') \
-                          --use-analyzer=${CLANG} make -j${NPROC} -C ${DIR}
+            ${SCAN_BUILD} -o "${TRESULTS_DIR}/$(echo "${DIR}" | sed 's/\//_/g')" \
+                          --use-analyzer="${CLANG}" make -j"${NPROC}" -C "${DIR}"
         fi
     done
 
     echo -e "\n| Analyzing with commit complete ... |\n"
 
-    rm -f ${REPORTS_DIR}/${PATCH_NAME}
+    rm -f "${REPORTS_DIR}/${PATCH_NAME}"
 }
 
 function count_for_baseline () {
-    for DIR in ${FILES[@]}; do
-        HTMLS_DIR=${BRESULTS_DIR}/$(echo ${DIR} |
-                    sed 's/\//_/g')/$(ls ${BRESULTS_DIR}/$(echo ${DIR} |
-                    sed 's/\//_/g')/);
+    for DIR in "${FILES[@]}"; do
+        HTMLS_DIR=${BRESULTS_DIR}/$(echo "${DIR}" |
+                    sed 's/\//_/g')/$(ls "${BRESULTS_DIR}"/"$(echo "${DIR}" |
+                    sed 's/\//_/g')"/);
 
-        local NAMES_OF_BUGS_B=$(grep -n "SUMM_DESC" ${HTMLS_DIR}/index.html |
+        local NAMES_OF_BUGS_B=$(grep -n "SUMM_DESC" "${HTMLS_DIR}/index.html" |
                                 cut -d"<" -f3 | cut -d">" -f2 |
                                 sed 's/[^a-zA-Z0]/_/g' | tr '\n' ' ')
-        local NO_OF_BUGS_B=$(grep -n "SUMM_DESC" ${HTMLS_DIR}/index.html |
+        local NO_OF_BUGS_B=$(grep -n "SUMM_DESC" "${HTMLS_DIR}/index.html" |
                              cut -d"<" -f5 | cut -d">" -f2 | tr '\n' ' ')
         local count_B=0;
 
-        read -a BUG_NAME_B <<<$NAMES_OF_BUGS_B
-        read -a BUG_COUNT_B <<<$NO_OF_BUGS_B
-        for i in ${BUG_NAME_B[@]};
+        read -r -a BUG_NAME_B <<<"${NAMES_OF_BUGS_B}"
+        read -r -a BUG_COUNT_B <<<"${NO_OF_BUGS_B}"
+        for i in "${BUG_NAME_B[@]}";
         do
-            if [ ! -z ${DICT_B[$i]} ]; then
-                DICT_B[$i]=$(expr ${BUG_COUNT_B[count_B]} + ${DICT_B[$i]});
+            if [ -n "${DICT_B[$i]}" ]; then
+                DICT_B[$i]=$(expr "${BUG_COUNT_B[count_B]}" + "${DICT_B[$i]}");
             else
                 DICT_B+=([$i]=${BUG_COUNT_B[count_B]});
             fi
-            count_B=$(expr $count_B + 1)
+            count_B=$(expr "${count_B}" + 1)
         done
     done
 
     echo -e "\nBASELINE BUGS LIST (before applying patch):"
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    for key_B in ${!DICT_B[@]}; do
+    for key_B in "${!DICT_B[@]}"; do
         echo "${key_B} --> ${DICT_B[${key_B}]}" | sed 's/_/ /g' | tr -s ' '
     done
 }
 
 function count_for_target () {
-    for DIR in ${FILES[@]}; do
-        HTMLS_DIR=${TRESULTS_DIR}/$(echo ${DIR} |
-                    sed 's/\//_/g')/$(ls ${TRESULTS_DIR}/$(echo ${DIR} |
-                    sed 's/\//_/g')/);
+    for DIR in "${FILES[@]}"; do
+        HTMLS_DIR="${TRESULTS_DIR}/$(echo "${DIR}" |
+                    sed 's/\//_/g')/$(ls "${TRESULTS_DIR}/$(echo "${DIR}" |
+                    sed 's/\//_/g')"/)";
 
-        local NAME_OF_BUGS_T=$(grep -n "SUMM_DESC" ${HTMLS_DIR}/index.html |
+        local NAME_OF_BUGS_T=$(grep -n "SUMM_DESC" "${HTMLS_DIR}/index.html" |
                               cut -d"<" -f3 | cut -d">" -f2 |
                               sed 's/[^a-zA-Z0]/_/g'| tr '\n' ' ')
-        local NO_OF_BUGS_T=$(grep -n "SUMM_DESC" ${HTMLS_DIR}/index.html |
+        local NO_OF_BUGS_T=$(grep -n "SUMM_DESC" "${HTMLS_DIR}/index.html" |
                              cut -d"<" -f5 | cut -d">" -f2 | tr '\n' ' ')
         local count_T=0;
 
-        read -a BUG_NAME_T <<<$NAME_OF_BUGS_T
-        read -a BUG_COUNT_T <<<$NO_OF_BUGS_T
+        read -r -a BUG_NAME_T <<<"${NAME_OF_BUGS_T}"
+        read -r -a BUG_COUNT_T <<<"${NO_OF_BUGS_T}"
 
-        for i in ${BUG_NAME_T[@]};
+        for i in "${BUG_NAME_T[@]}" ;
         do
-            if [ ! -z ${DICT_T[$i]} ]; then
-                DICT_T[$i]=$(expr ${BUG_COUNT_T[count_T]} + ${DICT_T[$i]});
+            if [ -n "${DICT_T[$i]}" ]; then
+                DICT_T[$i]=$(expr "${BUG_COUNT_T[count_T]}" + "${DICT_T[$i]}");
             else
                 DICT_T+=([$i]=${BUG_COUNT_T[count_T]});
             fi
-            count_T=$(expr $count_T + 1)
+            count_T=$(expr "${count_T}" + 1)
         done
     done
 
     echo -e "\nTARGET BUGS LIST (after applying patch):"
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    for key_T in ${!DICT_T[@]}; do
+    for key_T in "${!DICT_T[@]}"; do
         echo "${key_T} --> ${DICT_T[${key_T}]}" | sed 's/_/ /g' | tr -s ' '
     done
 }
@@ -244,7 +244,7 @@ function array_contains () {
     local SEEKING=$1; shift
     local IN=1
     for ELEMENT; do
-        if [[ $ELEMENT == $SEEKING ]]; then
+        if [[ $ELEMENT == "${SEEKING}" ]]; then
             IN=0
             break
         fi
@@ -264,19 +264,19 @@ function main () {
     echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
     FLAG=0
-    for BUG in ${!DICT_T[@]}; do
-        array_contains $BUG "${!DICT_B[@]}"
+    for BUG in "${!DICT_T[@]}"; do
+        array_contains "${BUG}" "${!DICT_B[@]}"
         if [ $? -eq 1 ]; then
-            echo "New ${DICT_T[${BUG}]} Bug[s] introduced: $(echo $BUG |
+            echo "New ${DICT_T[${BUG}]} Bug[s] introduced: $(echo "${BUG}" |
                                                             sed 's/_/ /g' |
                                                             tr -s ' ')"
             FLAG=1
         else
-            if [ ${BUG} != "All_Bugs" ]; then
-                if [ ${DICT_B[${BUG}]} -lt            \
-                    ${DICT_T[${BUG}]} ]; then
-                    echo "Extra $(expr ${DICT_T[${BUG}]} - \
-                          ${DICT_B[${BUG}]}) Bug[s] Introduced in: $(echo $BUG |
+            if [ "${BUG}" != "All_Bugs" ]; then
+                if [ "${DICT_B[${BUG}]}" -lt            \
+                    "${DICT_T[${BUG}]}" ]; then
+                    echo "Extra $(expr "${DICT_T[${BUG}]}" - \
+                          "${DICT_B[${BUG}]}") Bug[s] Introduced in: $(echo "${BUG}" |
                           sed 's/_/ /g' | tr -s ' ')"
                     FLAG=1
                 fi
@@ -291,8 +291,8 @@ function main () {
         echo -e "Patch Value given by Clang analyzer '-1'\n"
     fi
     echo -e "\nExplore complete results at:"
-    find ${BRESULTS_DIR}/ -iname "index.html"
-    find ${TRESULTS_DIR}/ -iname "index.html"
+    find "${BRESULTS_DIR}/" -iname "index.html"
+    find "${TRESULTS_DIR}/" -iname "index.html"
     echo -e "\n================= Done with Clang Analysis =================\n"
 
     exit ${FLAG}
