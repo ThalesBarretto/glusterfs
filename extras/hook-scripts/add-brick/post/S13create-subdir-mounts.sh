aafd5fb49a148ef12c780e47f7fac5414812be73
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-03-06 22:44:53 -0300

    shellcheck linter: use bash extras/hook-scripts/add-brick/post/S13create-subdir-mounts.sh
    	This commit changes the shell to #!/usr/bin/env bash.
    
            This series aim to reduce the shellcheck logs.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>

--- extras/hook-scripts/add-brick/post/S13create-subdir-mounts.sh
+++ extras/hook-scripts/add-brick/post/S13create-subdir-mounts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ##---------------------------------------------------------------------------
 ## This script runs the self-heal of the directories which are expected to
