7924a1ed8b3bf4d0f9425de75b51cc1be4986173
Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
2025-03-07 16:47:02 -0300

    shellcheck linter: use bash extras/quota/contri-add.sh
    	This commit changes the shell to #!/usr/bin/env bash.
    
            This series aim to reduce the shellcheck logs.
    
    Signed-off-by: Thales Antunes de Oliveira Barretto <thales.barretto.git@gmail.com>
    #  extras/quota/contri-add.sh | 26 +++++++++++++-------------
    #  1 file changed, 13 insertions(+), 13 deletions(-)
    #  extras/quota/contri-add.sh | 26 +++++++++++++-------------
    #  1 file changed, 13 insertions(+), 13 deletions(-)
    #  extras/quota/contri-add.sh | 26 +++++++++++++-------------
    #  1 file changed, 13 insertions(+), 13 deletions(-)

--- extras/quota/contri-add.sh
+++ extras/quota/contri-add.sh
@@ -20,20 +20,20 @@ add_contributions ()
     local var=0
     local count=0
 
-    SIZE=`getfattr -h -e hex -n trusted.glusterfs.quota.size $2 2>&1 | sed -e '/^#/d' | sed -e '/^getfattr/d' | sed -e '/^$/d' | cut -d'=' -f 2`
-    CONTRI=`getfattr -h -e hex -d -m trusted.glusterfs.quota.*.contri $2 2>&1 | sed -e '/^#/d' | sed -e '/^getfattr/d' | sed -e '/^$/d' | cut -d'=' -f 2`
+    SIZE=$(getfattr -h -e hex -n trusted.glusterfs.quota.size "$2" 2>&1 | sed -e '/^#/d' | sed -e '/^getfattr/d' | sed -e '/^$/d' | cut -d'=' -f 2)
+    CONTRI=$(getfattr -h -e hex -d -m trusted.glusterfs.quota.*.contri "$2" 2>&1 | sed -e '/^#/d' | sed -e '/^getfattr/d' | sed -e '/^$/d' | cut -d'=' -f 2)
 
-    if [ $1 == "file" ]; then
-        PATHS=`find  $2 ! -type d | sed -e "\|^$2$|d" | sed -e '/^[ \t]*$/d'`
+    if [ "$1" == "file" ]; then
+        PATHS=$(find  "$2" ! -type d | sed -e "\|^$2$|d" | sed -e '/^[ \t]*$/d')
     else
-        PATHS=`find  $2 -maxdepth 1 | sed -e "\|^$2$|d" | sed -e '/^[ \t]*$/d'`
+        PATHS=$(find  "$2" -maxdepth 1 | sed -e "\|^$2$|d" | sed -e '/^[ \t]*$/d')
     fi
 
     if [ -z "$PATHS" ]; then
         return 0
     fi
 
-    CONTRIBUTIONS=`echo $PATHS | xargs getfattr -h -e hex -d -m trusted.glusterfs.quota.*.contri 2>&1 | sed -e '/^#/d' | sed -e '/^getfattr/d' | sed -e '/^$/d' | cut -d'=' -f 2 | sed -e 's/^[ \t]*\([^ \t]*\)/\1/g'`
+    CONTRIBUTIONS=$(echo "$PATHS" | xargs getfattr -h -e hex -d -m trusted.glusterfs.quota.*.contri 2>&1 | sed -e '/^#/d' | sed -e '/^getfattr/d' | sed -e '/^$/d' | cut -d'=' -f 2 | sed -e 's/^[ \t]*\([^ \t]*\)/\1/g')
 
     if [ -n "$CONTRIBUTIONS" ]; then
         for i in $CONTRIBUTIONS; do
@@ -42,10 +42,10 @@ add_contributions ()
         done
     fi
 
-    if [ $1 == "file" ] || [ $var -ne $(($SIZE)) ] || [ $(($SIZE)) -ne $(($CONTRI)) ]; then
-        if [ $1 == "dir" ]; then
-            TMP_PATH=`echo $2 | sed -e "s/\/home\/export\/[0-9]*/\/mnt\/raghu/g"`
-            stat $TMP_PATH > /dev/null
+    if [ "$1" == "file" ] || [ $var -ne $(($SIZE)) ] || [ $(($SIZE)) -ne $(($CONTRI)) ]; then
+        if [ "$1" == "dir" ]; then
+            TMP_PATH=`echo "$2" | sed -e "s/\/home\/export\/[0-9]*/\/mnt\/raghu/g"`
+            stat "$TMP_PATH" > /dev/null
         fi
 
         echo "file count $count"
@@ -65,8 +65,8 @@ main ()
 
     shift 1
 
-    for i in $@; do
-        add_contributions $TYPE $i
+    for i in "$@"; do
+        add_contributions "${TYPE}" "${i}"
     done
 }
 
